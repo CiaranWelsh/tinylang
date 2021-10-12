22a1e6aaaf8e3f4e40227ab0e472ba5cfedf6313
Ciaran Welsh <cw00137@gmail.com>
2021-10-12 11:07:05 +0100

    Visitor for IR generation complete

---
+++ include/tinylang/Basic/CodeGen.h
@@ -0,0 +1,14 @@
+//
+// Created by Ciaran on 12/10/2021.
+//
+
+#ifndef TINYLANG_CODEGEN_H
+#define TINYLANG_CODEGEN_H
+
+
+class CodeGen {
+public:
+    void compile(AST* Tree);
+};
+
+#endif //TINYLANG_CODEGEN_H
